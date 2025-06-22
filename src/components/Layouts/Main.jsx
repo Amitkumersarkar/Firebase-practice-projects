87a690ff314a5aabe655c37a3c35196f61343011
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-06-23 01:19:55 +0600

    update file

--- src/components/Layouts/Main.jsx
+++ src/components/Layouts/Main.jsx
@@ -5,7 +5,7 @@ import Footer from "../Footer/Footer";
 const Main = () => {
 
     return (
-        <div>
+        <div className="w-11/12 mx-auto">
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
